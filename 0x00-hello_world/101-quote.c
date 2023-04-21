76586252b62168e7c21677b4ee375b8ea8f347b0
josiekarimi <josphinekarimi97@gmail.com>
2023-04-20 19:17:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,7 @@
-/**
-* main - prints a message to standard error
-*
-* Return: 1 always
-*/
+#include <unistd.h>
 int main(void)
 {
-/* entry point */
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, sizeof(message) - 1);
+return 1;
 }

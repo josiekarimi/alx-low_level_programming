6c39a0e9e75a44b490579edc0a9510cb4f89cb8b
josiekarimi <josphinekarimi97@gmail.com>
2023-04-20 18:09:04 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,15 @@
-#!/bin/bash
-gcc -Wall program.c -o program && ./program
+#include <stdio.h>
+
+/**
+*main - Entry point of the program
+ * This program demonstrates the use of printf()
+ *
+ * Return:Always 0 (success)
+ *0 - The program executed successfully.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
 

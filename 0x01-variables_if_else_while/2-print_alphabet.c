c9a8c7e7dcbd6ccff44206816ef09d798c35ffe1
josiekarimi <josphinekarimi97@gmail.com>
2023-04-24 13:06:24 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-/* This is the main function */
+/**
+* main - Prints all possible combinations of two two-digit numbers
+*
+* Return: Always 0
+*/
 int main(void)
 {
-// code goes here
-return 0;
+int i;
+for (i = 97; i < 123; i++)
+{
+if (i != 101 && i != 113)
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
 }

c3887ceb3eac92cc0a86b7047f10afab651242b9
josiekarimi <josphinekarimi97@gmail.com>
2023-04-24 12:53:42 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
-* main - prints all possible different combinations of three digits
+* main - Prints all possible combinations of two two-digit numbers
+*
 * Return: Always 0
 */
 int main(void)
@@ -24,3 +25,4 @@ putchar(' ');
 }
 putchar('\n');
 return (0);
+}

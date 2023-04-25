cd8f2b7d384bdf770ded24b78474baaea7e011ed
josiekarimi <josphinekarimi97@gmail.com>
2023-04-25 12:18:50 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,13 @@
-#include <stdio.h>
-#include "main.h"
 /**
-* main - prints all possible different combinations of three digits
-* Return: Always 0
-*/x
-/**
-* print_alphabet - prints the alphabet in lowercase followed by a new line
+* print_alphabet - prints the alphabet in lowercase, followed by a newline
 */
 void print_alphabet(void)
 {
-char c = 'a';
-while (c <= 'z')
+char c;
+for (c = 'a'; c <= 'z'; c++)
 {
-putchar(c);
-c++;
+_putchar(c);
 }
-putchar('\n');
+_putchar('\n');
 }
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+

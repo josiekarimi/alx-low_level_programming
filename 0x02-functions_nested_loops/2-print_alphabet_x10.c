319368a1c40853d13310c89d809febad5a05c40b
josiekarimi <josphinekarimi97@gmail.com>
2023-04-25 14:25:55 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,18 @@
 #include <unistd.h>
-
+/**
+* _putchar - writes a character to stdout
+* @c: the character to print
+*
+* Return: On success 1, on error -1 and errno set appropriately.
+*/
 int _putchar(char c)
 {
 return write(1, &c, 1);
 }
+/**
+* print_alphabet_x10 - prints the alphabet in lowercase 10 times,
+* followed by a new line
+*/
 void print_alphabet_x10(void)
 {
 char c;
@@ -17,6 +26,11 @@ _putchar(c);
 _putchar('\n');
 }
 }
+/**
+* main - entry point
+*
+* Return: always (0)
+*/
 int main(void)
 {
 print_alphabet_x10();

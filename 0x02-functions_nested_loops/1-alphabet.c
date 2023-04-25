eebd63ecdb5d46252a6d2ce3222698d1f6fe02da
josiekarimi <josphinekarimi97@gmail.com>
2023-04-25 13:47:05 -0700

    comm

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,16 +14,3 @@ putchar(c);
 }
 putchar('\n');
 }
-
-/* main function */
-/**
-* main - entry point of the program
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-

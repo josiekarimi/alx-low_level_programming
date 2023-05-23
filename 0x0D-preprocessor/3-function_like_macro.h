9993412eb30aea01819e8288e5c37293e34f78cf
josiekarimi <josphinekarimi97@gmail.com>
2023-05-23 00:17:56 -0700

    sum.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef MYMACROS_H
+#define MYMACROS_H
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* MYMACROS_H */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef MYMACROS_H
+#define MYMACROS_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* MYMACROS_H */

97199739cc19cd7ba90a84a54cf734a9bd76bba9
josiekarimi <josphinekarimi97@gmail.com>
2023-06-22 02:59:56 -0700

    5-flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns the number of bits
+ * @n: The first number
+ * @m: The second number
+ *
+ * Return: The number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int diff = n ^ m;
+unsigned int count = 0;
+while (diff)
+{
+count += diff & 1;
+diff >>= 1;
+}
+return (count);
+}

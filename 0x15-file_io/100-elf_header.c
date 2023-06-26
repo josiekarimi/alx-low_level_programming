f2f04e7099fc2e226f7ff325c3b303c32f24907c
josiekarimi <josphinekarimi97@gmail.com>
2023-06-26 00:19:22 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,48 +1,66 @@
-#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 #include <elf.h>
-
-void print_elf_header(const Elf64_Ehdr *header) {
-printf("ELF Header:\n");
-printf("  Magic:   ");
-for (int i = 0; i < EI_NIDENT; i++) {
-printf("%02x ", header->e_ident[i]);
-}
-printf("\n");
-printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2MSB ? "big endian" : "little endian");
-printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
-printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-printf("  Type:                              0x%x\n", header->e_type);
-printf("  Entry point address:               0x%lx\n", header->e_entry);
-printf("\n");
-}
-int main(int argc, char *argv[]) {
-if (argc != 2) {
-fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-return (97);
-}
-int fd = open(argv[1], O_RDONLY);
-if (fd == -1)
+/**
+ * display_error - Prints an error message and exits with status code 98.
+ * @message: The error message to be displayed.
+ */
+void display_error(const char *message)
 {
-fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-return (98);
+dprintf(2, "%s\n", message);
+exit(98);
 }
-Elf64_Ehdr header;
-if (read(fd, &header, sizeof(header)) != sizeof(header)) {
-fprintf(stderr, "Error: Unable to read ELF header\n");
+
+/**
+ * read_elf_header - Reads and displays information from the ELF header.
+ * @filename: The name of the ELF file.
+ */
+void read_elf_header(const char *filename)
+{
+int fd, bytes_read;
+Elf32_Ehdr header;
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+display_error("Error opening file");
+bytes_read = read(fd, &header, sizeof(header));
+if (bytes_read != sizeof(header))
+display_error("Error reading ELF header");
+if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+header.e_ident[EI_MAG1] != ELFMAG1 ||
+header.e_ident[EI_MAG2] != ELFMAG2 ||
+header.e_ident[EI_MAG3] != ELFMAG3)
+display_error("Not an ELF file");
+printf("Magic: %02x %02x %02x %02x\n",
+header.e_ident[EI_MAG0],
+header.e_ident[EI_MAG1],
+header.e_ident[EI_MAG2],
+header.e_ident[EI_MAG3]);
+printf("Class: %d\n", header.e_ident[EI_CLASS]);
+printf("Data: %d\n", header.e_ident[EI_DATA]);
+printf("Version: %d\n", header.e_ident[EI_VERSION]);
+printf("OS/ABI: %d\n", header.e_ident[EI_OSABI]);
+printf("ABI Version: %d\n", header.e_ident[EI_ABIVERSION]);
+printf("Type: %d\n", header.e_type);
+printf("Entry point address: %x\n", header.e_entry);
 close(fd);
-return (98);
 }
-if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
-header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3) {
-fprintf(stderr, "Error: %s is not an ELF file\n", argv[1]);
-close(fd);
-return (98);
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+display_error("Usage: elf_header elf_filename");
 }
-print_elf_header(&header);
-close(fd);
+read_elf_header(argv[1]);
 return (0);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,7 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void error_exit(const char *msg, int code);
 void cp_file(const char *file_from, const char *file);
+void display_error(const char *message);
+void read_elf_header(const char *filename);
 
 #endif /* MAIN_H */
